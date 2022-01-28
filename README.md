a99f0b8265e5275d9b148495f5636070dc434f76
bmariem <mariem.borchani@gmail.com>
2022-01-28 17:14:06 +0100

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tripadvisor
+
+Using HTML, pure CSS and Vanilla JS to recreate Japan TripAdvisor Page :cherry_blossom:
+
+
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get Current year
+  let date = new Date();
+  let year = date.getFullYear();
+  document.getElementById('currentYear').innerHTML = year;
+
+  //Open Modal 
+  document.getElementById("form-button").addEventListener("click", () => {
+    document.getElementById("modal").classList.remove("modal-hidden");
+    document.getElementById("body").classList.add("body-no-scroll");
+  });
+
+  // Remove Modal
+  document.getElementById("cross").addEventListener("click", () => {
+    document.getElementById("modal").classList.add("modal-hidden");
+    document.getElementById("body").classList.remove("body-no-scroll");
+  });
+
+  // Submit form
+  document
+    .getElementById("form-container")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const data = {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        text: document.getElementById("text").value,
+      };
+      
+      if (
+        document.getElementById("firstname").value &&
+        document.getElementById("lastname").value &&
+        document.getElementById("email").value &&
+        document.getElementById("text").value
+      ) {
+        const response = await axios.post(
+          "https://back-tripadvisor-japan.herokuapp.com/form",
+          data
+        );
+        if (response.status === 200) {
+
+          document
+            .getElementById("message-sent")
+            .classList.remove("modal-hidden").
+
+
+          setTimeout(() => {
+            document
+              .getElementById("message-sent")
+              .classList.add("modal-hidden");
+            document.getElementById("modal").classList.add("modal-hidden");
+            document.getElementById("body").classList.remove("body-no-scroll");
+         
+          }, 3000);
+        } else {
+          console.log(response.error);
+        }
+      } else {
+        document
+          .getElementById("error-message")
+          .classList.remove("modal-hidden");
+
+        setTimeout(() => {
+          document
+            .getElementById("error-message")
+            .classList.add("modal-hidden");
+        }, 3000);
+      }
+    });
+});
